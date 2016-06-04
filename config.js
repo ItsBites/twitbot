7a031a02f191b70bb0bb42b78c059032c5f5f47b
esther.fang <estherfang09@gmail.com>
2016-06-04 14:39:10 -0400

    first-commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	gifhy:"dc6zaTOxFJmzC",
-	consumer_key: 'dYsch2bgO1KGMBlRokMhGq4Q4',
-	consumer_secret: 'wHcq59p6wpzPcVN3vp89PPxsURhWjTIFbciSx07d2U8to21i9r',
-	access_token: '2523733684-GEycS33IS6UKEPWX3mNsXBdvEiJM3BSXFRU31Hv',
-	access_token_secret: 'r1P7u4nJxGoxbEcspGqwtxtrNU4HwlCamlHo72uE1GoVp'
+	gifhy: "insert your own key"
+	consumer_key: "insert your own key",
+	consumer_secret: "insert your own key",
+	access_token: "insert your own key",
+	access_token_secret: "insert your own key"
 }
